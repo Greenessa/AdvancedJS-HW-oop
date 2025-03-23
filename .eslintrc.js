754e9473d6afa266f27a152f5f52204dc31b95fc
Margo <romanova184@yandex.ru>
2025-03-23 16:47:10 +0300

    Commit with test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,44 @@
-module.exports = {
-    'env': {
-      'browser': true,
-      'es2021': true,
-      'node': true
+  module.exports = {
+
+    env: {
+  
+      browser: true,
+  
+      es2021: true,
+  
+      node: true,
+
+      jest: true
+
+  
     },
-    'extends': 'eslint:recommended',
-    'overrides': [],
-    'parserOptions': {
-      'ecmaVersion': 'latest',
-      'sourceType': 'module'
+  
+    extends: "eslint:recommended",
+  
+    overrides: [
+  
+      {
+  
+        files: ["**/*/*.test.js"],
+  
+        plugins: ["jest"],
+  
+        extends: ["plugin:jest/recommended"],
+  
+        rules: { "jest/prefer-expect-assertions": "off" },
+  
+      },
+  
+    ],
+  
+    parserOptions: {
+  
+      ecmaVersion: "latest",
+  
+      sourceType: "module",
+  
     },
-    'rules': {}
-  }
+  
+    rules: {},
+  
+  };
--- src/js/baseClass.js
+++ src/js/baseClass.js
@@ -1,32 +1,34 @@
 export class Character {
     constructor(name) {
-        if (name.length < 2 && name.length > 10 ) {
+        if (name.length < 2 || name.length > 10 ) {
             throw new Error('В имени персонажа должно быть от 2 до 10 символов');       
-        };
+        }
+
         this.name = name;
         this.health = 100;
         this.level = 1;
-        this.attack = 10;
-        this.defence = 10;
+        this.attack = undefined;
+        this.defence = undefined;
         
     }
     
     levelUp() {
         if (this.health < 0) {
             throw new Error('Нельзя повысить левел умершего персонажа');
-        };
+        }
+
         this.level+=1;
         this.health = 100;
-        this.attack = this.attack * 1,2;
-        this.defence = this.defence * 1,2;
+        this.attack = this.attack * 1.2;
+        this.defence = this.defence * 1.2;
     }
 
     damage(points) {
-        if (this.health <= 0) {
-            throw new Error('Персонаж умер');
-        };
         if (this.health > 0) {
             this.health -= points * (1 - this.defence / 100)
-        };
+        }
+        if (this.health <= 0) {
+            throw new Error('Персонаж умер');
+        }
     }
 }
